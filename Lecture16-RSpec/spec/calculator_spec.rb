c95b67d5ab9bc1b5c0531dacb78d55473c5b0792
Phil Myers <prm5031@gmail.com>
2015-09-22 21:22:12 -0400

    Use different numbers in the tests
    A student left a comment that these tests were not good because 2+2=4, 2*2=4, 4-2=2, and 4/2=2. If a student had accidentally used the `*` operator in the `add` implementation or the `/` operator in `subtract`, these tests wouldn't catch that.

--- Lecture16-RSpec/spec/calculator_spec.rb
+++ Lecture16-RSpec/spec/calculator_spec.rb
@@ -5,11 +5,11 @@ describe Calculator do
   before { @calculator = Calculator.new('RSpec calculator')}
 
   it "should add 2 numbers correctly" do
-  	expect(@calculator.add(2, 2)).to eq 4 
+  	expect(@calculator.add(2, 3)).to eq 5 
   end
   	
   it "should subtract 2 numbers correctly" do
-   expect(@calculator.subtract(4, 2)).to eq 2 
+   expect(@calculator.subtract(6, 3)).to eq 3 
   end  
 
   it "should sum two odd numbers and become even" do
